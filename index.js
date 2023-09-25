32192f276d7a1982af62d6b9905b80d74340d020
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-25 21:43:59 +0600

    added description for this project:

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import fs from 'fs';
-import arr from './demo.js';
-
-console.log('Hello World!');
-
-console.log(fs);
-console.log(arr);
+/*
+ * Title : Uptime Monitoring Application
+ * Description : A RESTFul API to monitor up or down time of user defined links
+ * Author : Kishor Paroi
+ * Date : 2023/09/25
+ * Time : 9:42:10 PM
+ */
