26f85e052d74f59d933941593b4f29d3f1dedb06
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-10-03 11:24:02 +0600

    put method created

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -152,7 +152,72 @@ handler._users.get = (requestProperties, callback) => {
     }
 };
 handler._users.put = (requestProperties, callback) => {
-    callback(100);
+    console.log('🚀 ~ file: userHandler.js:155 ~ requestProperties:', requestProperties);
+    const firstName =
+        typeof requestProperties.body.firstName === 'string' &&
+        requestProperties.body.firstName.trim().length > 0
+            ? requestProperties.body.firstName
+            : false;
+    const lastName =
+        typeof requestProperties.body.lastName === 'string' &&
+        requestProperties.body.lastName.trim().length > 0
+            ? requestProperties.body.lastName
+            : false;
+    const phone =
+        typeof requestProperties.body.phone === 'string' &&
+        requestProperties.body.phone.trim().length === 11
+            ? requestProperties.body.phone
+            : false;
+
+    const password =
+        typeof requestProperties.body.password === 'string' &&
+        requestProperties.body.password.trim().length > 0
+            ? requestProperties.body.password
+            : false;
+
+    if (phone) {
+        if (firstName || lastName || password) {
+            data.read('users', phone, (err, uData) => {
+                const userData = uData;
+                if (!err && userData) {
+                    if (firstName) {
+                        userData.firstName = firstName;
+                    }
+                    if (lastName) {
+                        userData.lastName = lastName;
+                    }
+                    if (password) {
+                        userData.password = utilities.hash(password);
+                    }
+
+                    // store in database
+                    data.update('users', phone, userData, (err2) => {
+                        if (!err2) {
+                            callback(200, {
+                                message: 'User was updated successfully',
+                            });
+                        } else {
+                            callback(500, {
+                                error: 'There was problem from Server Side',
+                            });
+                        }
+                    });
+                } else {
+                    callback(400, {
+                        error: 'You have problem in your request',
+                    });
+                }
+            });
+        } else {
+            callback(400, {
+                error: 'You have problem in your request',
+            });
+        }
+    } else {
+        callback(400, {
+            error: 'Invalid phone number, try again',
+        });
+    }
 };
 handler._users.delete = (requestProperties, callback) => {
     callback(300);
