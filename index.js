b2b6283d70f0d2200172df8d1b97b59bcff04928
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-10-11 14:00:38 +0600

    refactor index.js and divided works between worker.js and server.js

--- index.js
+++ index.js
@@ -7,70 +7,21 @@
  */
 
 // Dependencies
-import http from 'http';
-import environment from './helpers/environmentVariables.js';
-import handler from './helpers/handleReqRes.js';
-import notifications from './helpers/notifications.js';
-import data from './lib/data.js';
+import server from './lib/server.js';
+import workers from './lib/worker.js';
 
 // App Object - Module Scaffolding
 const app = {};
-// ! To Do : Delete kore dite hobe test korar pore
-// Testing Purpose
 
-notifications.sendTwilioSms('01724870810', 'Hello Kishor', (err) => {
-    console.log('THis is the error : ', err);
-});
+app.init = () => {
+    // start the server
+    server.init();
 
-console.log(data);
-console.log(process.cwd);
-
-// data.create(
-//     'test',
-//     'newFile',
-//     {
-//         name: 'Kishor',
-//         title: 'Paroi',
-//         middleName: 'Kumar',
-//     },
-//     (err) => {
-//         console.log(err, 'Kishor');
-//     }
-// );
-
-data.read('test', 'newFile', (err, result) => {
-    console.log(err, result);
-});
-
-// data.update(
-//     'test',
-//     'newFile',
-//     {
-//         name: 'Kishor Kumar Paroi',
-//         age: 24,
-//         dream: 'AI Developer',
-//         salary: '1M Dollar',
-//     },
-//     (err) => {
-//         console.log(err);
-//     }
-// );
-
-// data.delete('test', 'newFile', (err) => {
-//     console.log(err);
-// });
-
-// Create Server
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environment.port, () => {
-        console.log('Environment variable : ', environment.port);
-        console.log(`Listening to port ${environment.port}`);
-    });
+    // start the  workers
+    workers.init();
 };
 
-// Handle Request and Response
-app.handleReqRes = handler.handleReqRes;
-console.log('process.cwd() : ', process.cwd());
-// Start the server
-app.createServer();
+app.init();
+
+// export module
+export default app;
